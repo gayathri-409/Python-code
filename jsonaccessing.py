6aa6e8994e9cabfb561657edefb1b89dcd4c611a
gayathri-409 <52617701+gayathri-409@users.noreply.github.com>
2019-09-21 21:20:19 +0530

    Add files via upload

---
+++ jsonaccessing.py
@@ -0,0 +1,31 @@
+import json
+
+data='''{"glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}'''
+print(data)
+
+df = json.loads(data)
+for i in df['glossary']['GlossDiv']['GlossList']['GlossEntry']:
+     print(i)
+print(df['glossary']['GlossDiv']['GlossList']['GlossEntry']['GlossTerm'])
+
+
